56dd366b8adb5ffef86eafa5d1e818c50b42cff1
Xuloh <pacome.lehoux@laposte.net>
2018-02-21 17:14:32 +0000

    added a basic input system
    it features 3 different keymaps : azerty, qwerty and custom
    the controls of the custom keymap are remappable
    the active keymap can be changed at anytime
    (controllers not supported yet)

---
+++ lib/input/input-system.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <SFML/Window.hpp>
+#include <renderer-system.h>
+#include "input-system.h"
+
+using namespace std;
+using namespace sf;
+
+static set<string> controls;
+
+static unordered_map<string, Keyboard::Key> azertyKeymap;
+static unordered_map<string, Keyboard::Key> qwertyKeymap;
+static unordered_map<string, Keyboard::Key> customKeymap;
+
+// default keymap is qwerty
+static const unordered_map<string, Keyboard::Key>* activeKeymap = &qwertyKeymap;
+
+void input::addControl(const string& control, const Keyboard::Key defaultAzerty, const Keyboard::Key defaultQwerty) {
+	controls.insert(control);
+	azertyKeymap[control] = defaultAzerty;
+	qwertyKeymap[control] = defaultQwerty;
+}
+
+bool input::bindKey(const string& control, const Keyboard::Key key) {
+	if(controls.find(control) != controls.end()) {
+		for(const auto& pair : customKeymap)
+			if (pair.second == key)
+				return false; // the key is already bound to another control
+		customKeymap[control] = key;
+		return true; // the key was bound
+	}
+	return false; // the control is not registered
+}
+
+void input::unbindKey(const string& control) {
+	customKeymap.erase(control);
+}
+
+void input::setAzertyActive() {
+	activeKeymap = &azertyKeymap;
+}
+
+void input::setQwertyActive() {
+	activeKeymap = &qwertyKeymap;
+}
+
+void input::setCustomActive() {
+	activeKeymap = &customKeymap;
+}
+
+bool input::isControlPressed(const string& control) {
+	return renderer::getWindow().hasFocus() && Keyboard::isKeyPressed(activeKeymap->at(control));
+}
