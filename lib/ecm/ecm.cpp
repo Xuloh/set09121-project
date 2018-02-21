014f2163f15057528d37131fd8363d3fe31858ec
Xuloh <pacome.lehoux@laposte.net>
2018-02-21 11:29:19 +0000

    base ECM library
    - added a namespace declaring the Entity, Component and Scene classes and the
      EntityManager struct
    - added the Entity class
    - added the Component class

---
+++ lib/ecm/ecm.cpp
@@ -0,0 +1,80 @@
+#include "ecm.h"
+
+using namespace sf;
+using namespace std;
+using namespace ecm;
+
+// *** Entity class *** //
+
+Entity::Entity() : rotation(0.f), alive(true), visible(true), forDeletion(false) {}
+
+Entity::~Entity() = default;
+
+void Entity::update(const double dt) {
+	auto it = components.begin();
+	while(it != components.end()) {
+		if ((*it)->isForDeletion())
+			it = components.erase(it);
+		else
+			(*it++)->update(dt);
+	}
+}
+
+void Entity::render() {
+	for (const auto& component : components)
+		component->render();
+}
+
+const Vector2f& Entity::getPosition() const {
+	return position;
+}
+
+void Entity::setPosition(const Vector2f& position) {
+	this->position = position;
+}
+
+float Entity::getRotation() const {
+	return rotation;
+}
+
+void Entity::setRotation(const float rotation) {
+	this->rotation = rotation;
+}
+
+bool Entity::isAlive() const {
+	return alive;
+}
+
+void Entity::setAlive(const bool alive) {
+	this->alive = alive;
+}
+
+bool Entity::isVisible() const {
+	return visible;
+}
+
+void Entity::setVisible(const bool visible) {
+	this->visible = visible;
+}
+
+bool Entity::isForDeletion() const {
+	return forDeletion;
+}
+
+void Entity::setForDeletion() {
+	this->forDeletion = true;
+}
+
+// *** Component class *** //
+
+Component::Component(Entity* const parent) : parent(parent), forDeletion(false) {}
+
+Component::~Component() = default;
+
+bool Component::isForDeletion() const {
+	return forDeletion;
+}
+
+void Component::setForDeletion() {
+	forDeletion = true;
+}
---
+++ lib/ecm/ecm.h
@@ -0,0 +1,95 @@
+#pragma once
+#include <memory>
+#include <vector>
+#include <SFML/System.hpp>
+
+namespace ecm {
+	class Component;
+	class Entity;
+	enum EntityManager;
+	class Scene;
+}
+
+class ecm::Entity {
+protected:
+	std::vector<std::shared_ptr<Component>> components;
+
+	sf::Vector2f position;
+	float rotation;
+
+	bool alive; // whether the Entity should be updated
+	bool visible; // whether the Entity should be rendered
+	bool forDeletion; // whether the Entity should be deleted
+
+public:
+	Entity();
+	virtual ~Entity();
+
+	// update all the Entity's Components and delete those marked for deletion
+	virtual void update(double dt);
+	// render all the Entity's Components
+	virtual void render();
+
+	const sf::Vector2f& getPosition() const;
+	void setPosition(const sf::Vector2f& position);
+
+	float getRotation() const;
+	void setRotation(float rotation);
+
+	bool isAlive() const;
+	void setAlive(bool alive);
+
+	bool isVisible() const;
+	void setVisible(bool visible);
+
+	bool isForDeletion() const;
+	void setForDeletion();
+
+	// adds a Component of the given type to the Entity, passing any given parameters to the Component's constructor
+	template<typename T, typename... Targs> std::shared_ptr<T> addComponent(Targs... params) {
+		static_assert(std::is_base_of<Component, T>::value, "given type is not a Component");
+		std::shared_ptr<T> component = std::make_shared<T>(this, params...);
+		components.push_back(component);
+		return component;
+	}
+
+	// returns a pointer to the first Component of the given type that was found, or an empty pointer if none was found
+	template<typename T> std::shared_ptr<T> getComponent() {
+		static_assert(std::is_base_of<Component, T>::value, "given type is not a Component");
+		for (const auto& component : components) {
+			std::shared_ptr<T> castComponent = std::dynamic_pointer_cast<T>(component);
+			if (castComponent && castComponent.use_count() != 0)
+				return castComponent;
+		}
+		return std::shared_ptr<T>(nullptr);
+	}
+
+	// returns a vector of pointers to all the Components of the given type, or an empty vector if none were found
+	template<typename T> std::vector<std::shared_ptr<T>> getAllComponents() {
+		static_assert(std::is_base_of<Component, T>::value, "given type is not a Component");
+		std::vector<std::shared_ptr<T>> components;
+		for (const auto& component : this->components) {
+			std::shared_ptr<T> castComponent = std::dynamic_pointer_cast<T>(component);
+			if (castComponent && castComponent.use_count() != 0)
+				components.push_back(castComponent);
+		}
+		return components;
+	}
+};
+
+class ecm::Component {
+protected:
+	Entity * const parent;
+	bool forDeletion; // should be deleted
+	explicit Component(Entity *const parent);
+
+public:
+	Component() = delete;
+	virtual ~Component();
+
+	bool isForDeletion() const;
+	void setForDeletion();
+
+	virtual void update(double dt) = 0;
+	virtual void render() = 0;
+};
