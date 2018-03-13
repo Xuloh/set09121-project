c7ce5f7a0b86541f056e4a6729da6c699d56bf93
Xuloh <pacome.lehoux@laposte.net>
2018-03-13 17:07:28 +0000

    completely modified the gui system
    it now works as a set of Components that must be bound to an Entity to create a GUI element
    for example : a TextComponent and a ClickComponent (and optionaly a MouseHoverComponent) can make an Entity into a simple button

--- lib/gui/gui.cpp
+++ lib/gui/gui.cpp
@@ -6,118 +6,62 @@ using namespace sf;
 using namespace std;
 using namespace event;
 
-// *** GUIElement class *** //
+// *** TextComponent class *** //
 
-GUIElement::GUIElement(const FloatRect& hitArea) : hitArea(make_shared<FloatRect>(hitArea)) {}
+TextComponent::TextComponent(ecm::Entity* parent) : Component(parent) {}
 
-void GUIElement::handleEvent(const Event& event) {
-	switch(event.type) {
-		case Event::MouseMoved:
-			if(!mouseInArea && hitArea->contains(event.mouseMove.x, event.mouseMove.y)) {
-				mouseInArea = true;
-				onMouseEnter(event);
-			}
-			else if(mouseInArea && !hitArea->contains(event.mouseMove.x, event.mouseMove.y)) {
-				mouseInArea = false;
-				onMouseExit(event);
-			}
-			break;
-		case Event::MouseButtonReleased:
-			if(mouseInArea && event.mouseButton.button == Mouse::Left)
-				onClick(event);
-			break;
-		default:
-			break;
-	}
+void TextComponent::update(double dt) {
+	text->setPosition(parent->getPosition());
 }
 
-// *** Label class *** //
-
-Label::Label(const FloatRect& hitArea) : GUIElement(hitArea) {}
-
-void Label::render() {
+void TextComponent::render() {
 	renderer::queue(text.get());
 }
 
-// a label doesn't react to any event
-void Label::onMouseEnter(const Event& event) {}
+Text& TextComponent::getText() const {
+	return *text;
+}
 
-void Label::onMouseExit(const Event& event) {}
+// *** ClickComponent class *** //
 
-void Label::onClick(const Event& event) {}
-
-void Label::handleEvent(const Event& event) {}
-
-// *** Button class *** //
-
-Button::Button(const FloatRect& hitArea) : GUIElement(hitArea) {
-	registerHandler(Event::MouseMoved, this);
+ClickComponent::ClickComponent(ecm::Entity* parent) : Component(parent), targetText(parent->getComponent<TextComponent>()->getText()), onClick(nullptr) {
 	registerHandler(Event::MouseButtonReleased, this);
 }
 
-Button::~Button() {
-	unregisterHandler(Event::MouseMoved, this);
+ClickComponent::~ClickComponent() {
 	unregisterHandler(Event::MouseButtonReleased, this);
-
-	delete &hitArea;
-	delete &baseColor;
-	delete &hoverColor;
-	delete &text;
 }
 
-
-void Button::render() {
-	renderer::queue(text.get());
+void ClickComponent::handleEvent(const sf::Event& event) {
+	if (onClick != nullptr && event.mouseButton.button == Mouse::Left && targetText.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+		onClick(this, event);
 }
 
-void Button::onMouseEnter(const Event& event) {
-	text->setFillColor(*hoverColor);
+void ClickComponent::update(double dt) {}
+
+void ClickComponent::render() {}
+
+// *** MouseHoverComponent class *** //
+
+MouseHoverComponent::MouseHoverComponent(ecm::Entity* parent) : Component(parent), targetText(parent->getComponent<TextComponent>()->getText()), isMouseInArea(false) {
+	registerHandler(Event::MouseMoved, this);
 }
 
-void Button::onMouseExit(const Event& event) {
-	text->setFillColor(*baseColor);
+MouseHoverComponent::~MouseHoverComponent() {
+	unregisterHandler(Event::MouseMoved, this);
 }
 
-void Button::onClick(const Event& event) {
-	if(onClickHandler != nullptr)
-		onClickHandler(event);
+void MouseHoverComponent::handleEvent(const sf::Event& event) {
+	if (!isMouseInArea && targetText.getGlobalBounds().contains(event.mouseMove.x, event.mouseMove.y)) {
+		isMouseInArea = true;
+		targetText.setFillColor(hoverColor);
+	}
+	else if (isMouseInArea && !targetText.getGlobalBounds().contains(event.mouseMove.x, event.mouseMove.y)) {
+		isMouseInArea = false;
+		targetText.setFillColor(baseColor);
+	}
 }
 
-// *** GUIFactory class *** //
+void MouseHoverComponent::update(double dt) {}
 
-GUIFactory::GUIFactory(const float baseCharacterSize) : baseCharacterSize(baseCharacterSize) {}
-
-std::shared_ptr<Label> GUIFactory::makeLabel(const string& text, const sf::Vector2f& position, float characterSize) const {
-	auto label = make_shared<Label>();
-
-	label->text = make_shared<Text>();
-	label->text->setFont(*font);
-	label->text->setCharacterSize(baseCharacterSize * characterSize);
-	label->text->setFillColor(*baseColor);
-	label->text->setString(text);
-	label->text->setPosition(position);
-
-	label->hitArea = make_shared<FloatRect>(label->text->getGlobalBounds());
-
-	return label;
-}
-
-shared_ptr<Button> GUIFactory::makeButton(const string& text, const Vector2f& position, const eventFunction onClickHandler, const float characterSize) const {
-	auto button = make_shared<Button>();
-	
-	button->baseColor = baseColor;
-	button->hoverColor = hoverColor;
-	
-	button->onClickHandler = onClickHandler;
-
-	button->text = make_shared<Text>();
-	button->text->setFont(*font);
-	button->text->setCharacterSize(baseCharacterSize * characterSize);
-	button->text->setFillColor(*baseColor);
-	button->text->setString(text);
-	button->text->setPosition(position);
-
-	button->hitArea = make_shared<FloatRect>(button->text->getGlobalBounds());
-
-	return button;
-}
+void MouseHoverComponent::render() {}
