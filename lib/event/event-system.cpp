724bf835b0884f2dc333df14524fb04e661539f9
Xuloh <pacome.lehoux@laposte.net>
2018-03-13 14:42:12 +0000

    added checks on unregistering and handler execution to check if an event type has handlers

--- lib/event/event-system.cpp
+++ lib/event/event-system.cpp
@@ -7,26 +7,28 @@ using namespace std;
 using namespace event;
 
 static unordered_map<Event::EventType, set<eventFunction>> eventHandlersFunctions;
-static unordered_map<Event::EventType, set<EventHandler* const>> eventHandlers;
+static unordered_map<Event::EventType, set<EventHandler*>> eventHandlers;
 
 void event::registerHandler(const Event::EventType eventType, EventHandler* const eventHandler) {
 	if (!eventHandlers.count(eventType))
-		eventHandlers.insert({ eventType, set<EventHandler* const>() });
+		eventHandlers[eventType] = set<EventHandler*>();
 	eventHandlers[eventType].insert(eventHandler);
 }
 
 void event::unregisterHandler(const Event::EventType eventType, EventHandler* const eventHandler) {
-	eventHandlers[eventType].erase(eventHandler);
+	if(eventHandlers.count(eventType))
+		eventHandlers[eventType].erase(eventHandler);
 }
 
 void event::registerHandler(const Event::EventType eventType, const eventFunction eventHandler) {
 	if (!eventHandlersFunctions.count(eventType))
-		eventHandlersFunctions.insert({ eventType, set<eventFunction>() });
-	eventHandlersFunctions[eventType].insert(eventHandler);
+		eventHandlersFunctions[eventType] = set<eventFunction>();
+	eventHandlersFunctions.at(eventType).insert(eventHandler);
 }
 
 void event::unregisterHandler(const Event::EventType eventType, const eventFunction eventHandler) {
-	eventHandlersFunctions[eventType].erase(eventHandler);
+	if(eventHandlersFunctions.count(eventType))
+		eventHandlersFunctions[eventType].erase(eventHandler);
 }
 
 void event::unregisterAll() {
@@ -39,9 +41,11 @@ void event::update() {
 
 	Event event;
 	while (window.pollEvent(event)) {
-		for (auto& handler : eventHandlersFunctions[event.type])
-			handler(event);
-		for (auto& handler : eventHandlers[event.type])
-			handler->handleEvent(event);
+		if(eventHandlersFunctions.count(event.type))
+			for (const auto& handler : eventHandlersFunctions[event.type])
+				handler(event);
+		if(eventHandlers.count(event.type))
+			for (const auto& handler : eventHandlers[event.type])
+				handler->handleEvent(event);
 	}
 }
