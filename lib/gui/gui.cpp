96a6066863fe8d3aa6c7640046545f28924ab13d
Xuloh <pacome.lehoux@laposte.net>
2018-03-15 18:01:36 +0000

    added a LayoutComponent class to enable laying out GUI elements relative to the window's size

--- lib/gui/gui.cpp
+++ lib/gui/gui.cpp
@@ -102,3 +102,42 @@ void MouseHoverComponent::handleEvent(const sf::Event& event) {
 void MouseHoverComponent::update(double dt) {}
 
 void MouseHoverComponent::render() {}
+
+// *** LayoutComponent class *** //
+
+LayoutComponent::LayoutComponent(ecm::Entity* parent) : LayoutComponent(parent, 0.f, 0.f) {}
+LayoutComponent::LayoutComponent(ecm::Entity* parent, float x, float y) : Component(parent), x(x), y(y) {
+	updateSize();
+	updateLayout = true;
+	registerHandler(Event::Resized, this);
+}
+
+LayoutComponent::~LayoutComponent() {
+	unregisterHandler(Event::Resized, this);
+}
+
+void LayoutComponent::update(double dt) {
+	if (updateLayout) {
+		for (auto& item : items)
+			item.entity->setPosition(parent->getPosition() + Vector2f(item.x * width, item.y * height));
+		updateLayout = false;
+	}
+}
+
+void LayoutComponent::render() {}
+
+
+void LayoutComponent::handleEvent(const Event& event) {
+	updateLayout = true;
+	updateSize();
+}
+
+void LayoutComponent::addItem(std::shared_ptr<ecm::Entity> entity, float x, float y) {
+	items.push_back({ entity, x, y });
+}
+
+
+void LayoutComponent::updateSize() {
+	width = renderer::getWindow().getView().getSize().x * x;
+	height = renderer::getWindow().getView().getSize().y * y;
+}
