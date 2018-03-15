b57d0ce150f7ef02ecbea107f5631a0c014cd148
Xuloh <pacome.lehoux@laposte.net>
2018-03-15 16:08:00 +0000

    added a resize event handler to update the view when the window is resized

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <renderer-system.h>
 #include <input-system.h>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "Scenes.h"
 #include "main.h"
 
@@ -20,10 +21,15 @@ void closeWindowOnEscapePressed(const Event& event) {
 		closeWindow(event);
 }
 
+void resizeView(const Event& event) {
+	renderer::getWindow().setView(View({ 0.f, 0.f, float(event.size.width), float(event.size.height) }));
+}
+
 void load() {
 	// register window handlers
 	event::registerHandler(Event::Closed, &closeWindow);
 	event::registerHandler(Event::KeyReleased, &closeWindowOnEscapePressed);
+	event::registerHandler(Event::Resized, &resizeView);
 
 	font.loadFromFile("res/fonts/FiraCode-Medium.ttf");
 
@@ -48,7 +54,7 @@ void render() {
 }
 
 int main() {
-	RenderWindow window(VideoMode(800, 600), "MOIM", Style::Titlebar | Style::Close);
+	RenderWindow window(VideoMode(800, 600), "MOIM");
 	renderer::initialise(window);
 
 	load();
