ffbe58857e6027dec0d7410d154250d05bf7430e
Xuloh <pacome.lehoux@laposte.net>
2018-03-16 19:23:27 +0000

    added a physics system to handle a Box2D world and updated CMakeLists.txt accordingly

---
+++ lib/physics/physics-system.cpp
@@ -0,0 +1,25 @@
+#include "physics-system.h"
+
+using namespace std;
+using namespace sf;
+using namespace physics;
+
+static shared_ptr<b2World> world;
+const int32 velocityIterations = 8;
+const int32 positionIterations = 3;
+
+void physics::initialise(const b2Vec2 gravity) {
+	world.reset(new b2World(gravity));
+}
+
+void physics::shutdown() {
+	world.reset();
+}
+
+void physics::update(const double& dt) {
+	world->Step(float32(dt), velocityIterations, positionIterations);
+}
+
+shared_ptr<b2World> getWorld() {
+	return world;
+}
---
+++ lib/physics/physics-system.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <Box2D/Box2D.h>
+#include <SFML/System/Vector2.hpp>
+#include <memory>
+#include <renderer-system.h>
+
+namespace physics {
+
+	// initialise the physics system using the given gravity
+	void initialise(b2Vec2 gravity = { 0.f, -9.81f });
+	
+	// shutdowns the physics system
+	void shutdown();
+
+	// update the physics system
+	void update(const double& dt);
+
+	// returns a pointer to the Box2D world managed by the physics system
+	std::shared_ptr<b2World> getWorld();
+
+	// the scale used to convert meters to pixels
+	const float scale = 50.f;
+
+	// the scale used to convert pixels to meters
+	const float scaleInv = 1.f / scale;
+
+	// Converts Box2D coordinates to SFML coordinates
+	// it applies the physics scale and inverts the y coordinate
+	//
+	// Note : this function's name breaks the naming convention for readability purposes
+	// ReSharper disable once CppInconsistentNaming
+	inline sf::Vector2f b2Vec2_To_Vector2f(const b2Vec2& vector) {
+		return { vector.x * scale, renderer::getWindow().getSize().y - vector.y * scale };
+	}
+
+	// Converts SFML coordinates to Box2D coordinates
+	// it applies the inverse physics scale and inverts the y coordinate
+	//
+	// Note : this function's name breaks the naming convention for readability purposes
+	// ReSharper disable once CppInconsistentNaming
+	inline b2Vec2 Vector2f_To_b2Vec2(const sf::Vector2f vector) {
+		return { vector.x * scaleInv, renderer::getWindow().getSize().y - vector.y * scaleInv };
+	}
+
+	// Converts meters (used within Box2D) to pixels (used within SFML)
+	inline float metersToPixels(const float meters) {
+		return meters * scale;
+	}
+
+	// Converts pixels (used within SFML) to meters (used within Box2D)
+	inline float pixelsToMeters(const float pixels) {
+		return pixels * scaleInv;
+	}
+
+	// Converts radians (used within Box2D) to degrees (used within SFML)
+	inline float radiansToDegrees(const float radians) {
+		return -(radians * 180.f / b2_pi);
+	}
+
+	// Converts degrees (used within SFML) to radians (used within Box2D)
+	inline float degreesToRadians(const float degrees) {
+		return -(degrees * b2_pi / 180.f);
+	}
+}
