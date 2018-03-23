a9256c8b07f4b08370c87c5c556098572cc05211
Xuloh <pacome.lehoux@laposte.net>
2018-03-23 18:33:57 +0000

    modified the renderer system
    - it now handles 2 different Views (SFML 2D cameras) : 1 for the GUI and 1 for the actual scene content
    - the GUI view doesn't move and has the same coordinates as the window (coordinates (0,0) correspond to the upper left corner for both the window and the GUI view)
    - the scene view can be moved around to display whichever part of the world is relevant (for example to follow the player around)
    - the queue function now accept an optional parameter, forGUI, which when set to true queues the drawable for the GUI view instead of the scene view (it defaults to false)

--- engine/renderer/renderer-system.cpp
+++ engine/renderer/renderer-system.cpp
@@ -1,19 +1,35 @@
 #include <exception>
 #include <queue>
+#include <memory>
 #include "renderer-system.h"
 
 using namespace std;
 using namespace sf;
 
 static queue<const Drawable*> drawables;
+static queue<const Drawable*> gui;
 static RenderWindow* renderWindow;
+static shared_ptr<View> sceneView;
+static shared_ptr<View> guiView;
 
 RenderWindow& renderer::getWindow() {
 	return *renderWindow;
 }
 
-void renderer::initialise(sf::RenderWindow& window) {
+View& renderer::getSceneView() {
+    return *sceneView;
+}
+
+View& renderer::getGUIView() {
+    return *guiView;
+}
+
+void renderer::initialise(RenderWindow& window) {
 	renderWindow = &window;
+
+    const auto windowSize = Vector2f(window.getSize());
+    sceneView.reset(new View({ 0.f, -windowSize.y, windowSize.x, windowSize.y }));
+    guiView.reset(new View({ 0.f, 0.f, windowSize.x, windowSize.y }));
 }
 
 void renderer::shutdown() {
@@ -25,12 +41,22 @@ void renderer::render() {
 	if (renderWindow == nullptr)
 		throw exception("No render window set :(");
 
+    renderWindow->setView(*sceneView);
 	while (!drawables.empty()) {
 		renderWindow->draw(*drawables.front());
 		drawables.pop();
 	}
+
+    renderWindow->setView(*guiView);
+    while(!gui.empty()) {
+        renderWindow->draw(*gui.front());
+        gui.pop();
+    }
 }
 
-void renderer::queue(const Drawable* drawable) {
-	drawables.push(drawable);
+void renderer::queue(const Drawable* drawable, bool forGUI) {
+    if (!forGUI)
+        drawables.push(drawable);
+    else
+        gui.push(drawable);
 }
--- engine/renderer/renderer-system.h
+++ engine/renderer/renderer-system.h
@@ -6,13 +6,21 @@ namespace renderer {
 	// return a reference to the renderer's Window
 	sf::RenderWindow& getWindow();
 
+    // return a reference to the view used to render the scene
+    sf::View& getSceneView();
+
+    // return a reference to the view used to render the GUI
+    sf::View& getGUIView();
+
 	// initialise the renderer with the given Window
 	void initialise(sf::RenderWindow& window);
+
 	// shuts down the renderer
 	void shutdown();
 
 	// adds the given Drawable to the render queue
-	void queue(const sf::Drawable* drawable);
-	// renders all the Drawable that were queued and empties the queue
+	void queue(const sf::Drawable* drawable, bool forGUI = false);
+
+    // renders all the Drawable that were queued and empties the queue
 	void render();
 }
