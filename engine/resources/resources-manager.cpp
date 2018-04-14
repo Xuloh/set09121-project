74b1c890b01038cedb9db925173c0e25e5006b0a
Bran Flakes <branflakes@ymail.com>
2018-04-14 21:41:26 +0000

    Removed debug params Added sprites Added enemy AI

--- engine/resources/resources-manager.cpp
+++ engine/resources/resources-manager.cpp
@@ -5,6 +5,8 @@ using namespace sf;
 
 template<> shared_ptr<Font> resources::load(const string& name) {
     auto font = make_shared<Font>();
+	//uncomment for debug
+	//if (!font->loadFromFile("debug/res/fonts/" + name))
     if (!font->loadFromFile("res/fonts/" + name))
         throw string("Could not load font : " + name);
     return font;
@@ -12,6 +14,8 @@ template<> shared_ptr<Font> resources::load(const string& name) {
 
 template<> shared_ptr<Texture> resources::load(const string& name) {
     auto texture = make_shared<Texture>();
+	//Uncomment for debug
+	//if (!texture->loadFromFile("debug/res/sprites" + name))
     if (!texture->loadFromFile("res/sprites" + name))
         throw string("Could not load sprite : " + name);
     return texture;
