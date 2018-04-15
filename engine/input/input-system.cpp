6c5d62b21dc7b47e423e0a51f87e4627fe6434a6
Xuloh <pacome.lehoux@laposte.net>
2018-04-15 22:53:55 +0100

    modified the input system to support controller input

--- engine/input/input-system.cpp
+++ engine/input/input-system.cpp
@@ -9,13 +9,16 @@ using namespace std;
 using namespace sf;
 
 static set<string> controls;
-
 static unordered_map<string, Keyboard::Key> azertyKeymap;
 static unordered_map<string, Keyboard::Key> qwertyKeymap;
 static unordered_map<string, Keyboard::Key> customKeymap;
+static unordered_map<string, unsigned> joystickButtonsMap;
+static unordered_map<string, input::AxisControl> joystickAxisMap;
 
 // default keymap is qwerty
 static const unordered_map<string, Keyboard::Key>* activeKeymap = &qwertyKeymap;
+
+static auto useController = false;
 
 void input::addControl(const string& control, const Keyboard::Key defaultAzerty, const Keyboard::Key defaultQwerty) {
 	controls.insert(control);
@@ -38,6 +41,37 @@ void input::unbindKey(const string& control) {
 	customKeymap.erase(control);
 }
 
+bool input::bindJoystickButton(const string& control, const unsigned joystickButton) {
+    if (controls.find(control) != controls.end()) {
+        for (const auto& pair : joystickButtonsMap)
+            if (pair.second == joystickButton)
+                return false;
+        joystickButtonsMap[control] = joystickButton;
+        return true;
+    }
+    return false;
+}
+
+void input::unbindJoystickButton(const string& control) {
+    joystickButtonsMap.erase(control);
+}
+
+bool input::bindJoystickAxis(const std::string& control, const Joystick::Axis axis, const float min, const float max) {
+    if(controls.find(control) != controls.end()) {
+        const AxisControl axisControl = { axis, min, max };
+        for (const auto& pair : joystickAxisMap)
+            if (pair.second == axisControl)
+                return false;
+        joystickAxisMap[control] = axisControl;
+        return true;
+    }
+    return false;
+}
+
+void input::unbindJoystickAxis(const std::string& control) {
+    joystickAxisMap.erase(control);
+}
+
 void input::setAzertyActive() {
 	activeKeymap = &azertyKeymap;
 }
@@ -50,10 +84,44 @@ void input::setCustomActive() {
 	activeKeymap = &customKeymap;
 }
 
+bool input::usingController() {
+    return useController;
+}
+
+void input::setUseController(const bool useController) {
+    if (Joystick::isConnected(0))
+        ::useController = useController;
+    else
+        ::useController = false;
+}
+
 bool input::isControlPressed(const string& control) {
-	return controls.count(control) && renderer::getWindow().hasFocus() && Keyboard::isKeyPressed(activeKeymap->at(control));
+    if (controls.count(control) && renderer::getWindow().hasFocus()) {
+        if (!useController)
+            return Keyboard::isKeyPressed(activeKeymap->at(control));
+        if (joystickButtonsMap.count(control))
+            return Joystick::isButtonPressed(0, joystickButtonsMap[control]);
+        if(joystickAxisMap.count(control)) {
+            const auto& axisControl = joystickAxisMap[control];
+            const auto axisPosition = Joystick::getAxisPosition(0, axisControl.axis);
+            return axisPosition >= axisControl.min && axisPosition <= axisControl.max;
+        }
+    }
+    return false;
 }
 
 Keyboard::Key input::getKey(const string& control) {
 	return controls.count(control) ? activeKeymap->at(control) : Keyboard::Unknown;
+}
+
+unsigned input::getJoystickButton(const string& control) {
+    return controls.count(control) ? joystickButtonsMap[control] : Joystick::ButtonCount + 1;
+}
+
+bool input::getJoystickAxis(const string& control, AxisControl* axisControl) {
+    if(controls.count(control)) {
+        *axisControl = joystickAxisMap[control];
+        return true;
+    }
+    return false;
 }
--- engine/input/input-system.h
+++ engine/input/input-system.h
@@ -3,6 +3,19 @@
 #include <SFML/Window.hpp>
 
 namespace input {
+
+    // An axis control associates an axis with a minimum and maximum position
+    struct AxisControl {
+        sf::Joystick::Axis axis;
+        float min;
+        float max;
+    };
+
+    // test the equality of 2 AxisControls
+    inline bool operator ==(const AxisControl& a, const AxisControl& b) {
+        return a.axis == b.axis && a.min == b.min && a.max == b.max;
+    }
+
 	// register a new control name
 	void addControl(const std::string& control, sf::Keyboard::Key defaultAzerty, sf::Keyboard::Key defaultQwerty);
 	
@@ -13,6 +26,20 @@ namespace input {
 	// unbind whatever key was bound to control
 	void unbindKey(const std::string& control);
 	
+    // binds the given control in the joystick buttons map, the control must be registered with addControl
+    // returns true if the button was bound or false if the control was not registered or if the button is already bound to another control
+    bool bindJoystickButton(const std::string& control, unsigned joystickButton);
+
+    // unbind whatever joystick button was bound to control
+    void unbindJoystickButton(const std::string& control);
+
+    // binds the given control in the joystick axis map, the control must be registered with addControl
+    // returns true if the axis was bound or false if the control was not registered or if the axis is already bound to another control with the same min and max values
+    bool bindJoystickAxis(const std::string& control, sf::Joystick::Axis axis, float min, float max);
+
+    // unbind whatever joystick axis was bound to control
+    void unbindJoystickAxis(const std::string& control);
+
 	// set active keymap to azerty
 	void setAzertyActive();
 
@@ -22,12 +49,27 @@ namespace input {
 	// set active keymap to custom
 	void setCustomActive();
 
+    // returns true if currently using the controller as input or false if not
+    bool usingController();
+
+    // set wether the controller should be used as input or not
+    void setUseController(bool useController);
+
 	// determines if the given control is pressed
 	// returns false when the window does not have the focus
 	// also returns false if the given control does not exist
+    // if input is configured to use a controller, this function will check the state of the controller instead of the state of the keyboard
 	bool isControlPressed(const std::string& control);
 
 	// returns the key associated with the given control in the active keymap
 	// returns the unknown key sf::Keyboard::Unknown if the given control does not exist
 	sf::Keyboard::Key getKey(const std::string& control);
+
+    // return the joystick button associated with the given control
+    // return sf::Joystick::ButtonCount + 1 if the given control does not exist
+    unsigned getJoystickButton(const std::string& control);
+
+    // assigns the axis control associated with the given control to the given axis control pointer
+    // returns true if a value was actually assigned or returns false if the given control does not exist
+    bool getJoystickAxis(const std::string& control, AxisControl* axisControl);
 }
