add9689599f8b653db3d3edb7f38af49e4d9cb2c
Xuloh <pacome.lehoux@laposte.net>
2018-04-15 15:10:02 +0100

    modified the ClickComponent's event handling
    now it ignores events if its parent entity is not alive

--- engine/gui/gui.cpp
+++ engine/gui/gui.cpp
@@ -72,8 +72,8 @@ ClickComponent::~ClickComponent() {
 }
 
 void ClickComponent::handleEvent(const sf::Event& event) {
-	if (onClick != nullptr && event.mouseButton.button == Mouse::Left && targetText.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
-		onClick(event);
+	if (parent->isAlive() && onClick != nullptr && event.mouseButton.button == Mouse::Left && targetText.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+		(*onClick)(event);
 }
 
 void ClickComponent::update(double dt) {}
